500e05f9d92d4c4dbade865bfcb5139172d06a98
arcedo <arcedo.marc@gmail.com>
2026-06-23 16:55:43 +0200

    fix: some fixes :)

--- shared/scripts/power-menu/powermenu.sh
+++ shared/scripts/power-menu/powermenu.sh
@@ -5,8 +5,8 @@ dir="~/shared/scripts/power-menu/"
 theme='style'
 
 # CMDs
-uptime="`uptime -p | sed -e 's/up //g'`"
-host=`hostname`
+uptime="$(uptime -p | sed -e 's/up //g')"
+host=$(hostname)
 
 # Options
 shutdown=''
@@ -17,91 +17,107 @@ logout=''
 yes=''
 no=''
 
+# Screen control (Hyprland only)
+screen_off() {
+  hyprctl dispatch dpms off
+}
+
+screen_on() {
+  hyprctl dispatch dpms on
+}
+
 # Rofi CMD
 rofi_cmd() {
-	rofi -dmenu \
-		-p "Uptime: $uptime" \
-		-mesg "Uptime: $uptime" \
-		-theme ${dir}/${theme}.rasi
+  rofi -dmenu \
+    -p "Uptime: $uptime" \
+    -mesg "Uptime: $uptime" \
+    -theme ${dir}/${theme}.rasi
 }
 
 # Confirmation CMD
 confirm_cmd() {
-	rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 350px;}' \
-		-theme-str 'mainbox {children: [ "message", "listview" ];}' \
-		-theme-str 'listview {columns: 2; lines: 1;}' \
-		-theme-str 'element-text {horizontal-align: 0.5;}' \
-		-theme-str 'textbox {horizontal-align: 0.5;}' \
-		-dmenu \
-		-p 'Confirmation' \
-		-mesg 'Are you Sure?' \
-		-theme ${dir}/${theme}.rasi
+  rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 350px;}' \
+    -theme-str 'mainbox {children: [ "message", "listview" ];}' \
+    -theme-str 'listview {columns: 2; lines: 1;}' \
+    -theme-str 'element-text {horizontal-align: 0.5;}' \
+    -theme-str 'textbox {horizontal-align: 0.5;}' \
+    -dmenu \
+    -p 'Confirmation' \
+    -mesg 'Are you Sure?' \
+    -theme ${dir}/${theme}.rasi
 }
 
-# Ask for confirmation
 confirm_exit() {
-	echo -e "$yes\n$no" | confirm_cmd
+  echo -e "$yes\n$no" | confirm_cmd
 }
 
-# Pass variables to rofi dmenu
 run_rofi() {
-	echo -e "$lock\n$shutdown\n$reboot\n$suspend\n$logout" | rofi_cmd
+  echo -e "$lock\n$shutdown\n$reboot\n$suspend\n$logout" | rofi_cmd
 }
 
-# Execute Command
 run_cmd() {
-	selected="$(confirm_exit)"
-	if [[ "$selected" == "$yes" ]]; then
-		if [[ $1 == '--shutdown' ]]; then
-			systemctl poweroff
-		elif [[ $1 == '--reboot' ]]; then
-			systemctl reboot
-		elif [[ $1 == '--suspend' ]]; then
-			mpc -q pause
-			amixer set Master mute
-			systemctl suspend
-		elif [[ $1 == '--logout' ]]; then
-            if [[ "$DESKTOP_SESSION" == 'openbox' ]]; then
-                openbox --exit
-            elif [[ "$DESKTOP_SESSION" == 'bspwm' ]]; then
-                bspc quit
-            elif [[ "$DESKTOP_SESSION" == 'i3' ]]; then
-                i3-msg exit
-            elif [[ "$DESKTOP_SESSION" == 'plasma' ]]; then
-                qdbus org.kde.ksmserver /KSMServer logout 0 0 0
-            elif [[ "$DESKTOP_SESSION" == 'hyprland' ]]; then
-                hyprctl dispatch exit 1
-            fi
-        fi
-	else
-		exit 0
-	fi
+  selected="$(confirm_exit)"
+
+  [[ "$selected" != "$yes" ]] && exit 0
+
+  case "$1" in
+  --shutdown)
+    systemctl poweroff
+    ;;
+
+  --reboot)
+    systemctl reboot
+    ;;
+
+  --suspend)
+    mpc -q pause
+    amixer set Master mute
+    systemctl suspend
+    ;;
+
+  --logout)
+    hyprctl dispatch exit 1
+    ;;
+  esac
 }
 
-# Actions
 chosen="$(run_rofi)"
+
 case ${chosen} in
-    $shutdown)
-		run_cmd --shutdown
-        ;;
-    $reboot)
-		run_cmd --reboot
-        ;;
-    $lock)
-        if [[ -x '/usr/bin/hyprlock' ]]; then
-            hyprlock
-        elif [[ -x '/usr/bin/swaylock' ]]; then
-            swaylock
-        elif [[ -x '/usr/bin/betterlockscreen' ]]; then
-            betterlockscreen -l
-        elif [[ -x '/usr/bin/i3lock' ]]; then
-            i3lock
-        fi
-        ;;
-    $suspend)
-		run_cmd --suspend
-        ;;
-    $logout)
-		run_cmd --logout
-        ;;
+
+$shutdown)
+  run_cmd --shutdown
+  ;;
+
+$reboot)
+  run_cmd --reboot
+  ;;
+
+$lock)
+  # 🔥 Hyprland-optimized lock flow
+
+  # 1. Turn screen off immediately
+  screen_off
+
+  # 2. Small delay so DPMS registers before lock
+  sleep 0.2
+
+  # 3. Lock (this will wake the screen when active — expected behavior)
+  hyprlock &
+
+  # 4. Wait until lock starts fully
+  sleep 0.5
+
+  # 5. Optional: ensure DPMS stays off briefly (prevents flash)
+  screen_off
+  ;;
+
+$suspend)
+  run_cmd --suspend
+  ;;
+
+$logout)
+  run_cmd --logout
+  ;;
+
 esac
